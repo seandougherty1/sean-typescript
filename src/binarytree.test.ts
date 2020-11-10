edaedce1cec576df71284557855673c939b7d32b
seandougherty1 <15c16331@marling.school>
2020-11-10 14:23:28 +0000

    10/11/20 classwork

--- src/binarytree.test.ts
+++ src/binarytree.test.ts
@@ -1,4 +1,7 @@
 import BinaryTree from "comp-sci-maths-lib/dist/dataStructures/binaryTree/BinaryTree";
+import traverseInOrder from "comp-sci-maths-lib/dist/algorithms/binaryTreeTraversal/traverseInOrder"
+import traversePostOrder from "comp-sci-maths-lib/dist/algorithms/binaryTreeTraversal/traversePostOrder";
+import traversePreOrder from "comp-sci-maths-lib/dist/algorithms/binaryTreeTraversal/traversePreOrder"
 test('Binary tree', () => {
     let myTree = new BinaryTree<number>((a,b) => a - b);
     myTree.add(6)
@@ -10,4 +13,18 @@ test('Binary tree', () => {
     expect(myTree.contains(17)).toBeFalsy();
 })
 
+
+test('Binary tree in order', () => {
+    let myTreeOrder = new BinaryTree<number>((a,b) => a - b);
+    myTreeOrder.add(6)
+    myTreeOrder.add(9)
+    myTreeOrder.add(13)
+    myTreeOrder.add(19)
+    myTreeOrder.add(21)
+    myTreeOrder.add(7);
+    [].forEach(x => myTreeOrder.add)
+
+    expect(myTreeOrder.contains(9)).toBeTruthy();
+    expect(myTreeOrder.contains(17)).toBeFalsy();
+})
 
